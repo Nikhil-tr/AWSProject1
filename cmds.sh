ef9979534455dd5f120fec3bbbfe2e8fadd24a31
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-03-01 16:46:54 +0530

    Create cmds.sh

---
+++ cmds.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-flask  -y
+sudo apt install python3-pymysql -y
+sudo apt install python3-boto3  -y
+
+sudo apt install mysql-client -y
+sudo apt install python3-pymysql -y
